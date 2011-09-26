ebb775e148cce26d48ef335c4b151c8a5596e549
T S <YenTheFirst@gmail.com>
2011-09-25 18:59:05 -0700

    add auto-defaulting group name to search

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,4 +1,10 @@
 class Search < ActiveRecord::Base
   belongs_to        :user
   has_many          :listings
+
+  #group name can be overriden by us, or we can default to our user's group name, if we have a user.
+  def group_name
+    attributes["group_name"].presence || (user && user.group_name)
+  end
+
 end
