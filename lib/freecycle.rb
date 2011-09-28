6c6d03d764328caf515eb2e9ff8f2f900b017a0d
Michelle Koeth <michelle@codeforamerica.org>
2011-09-27 22:41:10 -0700

    Added some new functions in the library that will log u in

--- lib/freecycle.rb
+++ lib/freecycle.rb
@@ -2,6 +2,14 @@ require 'net/http'
 
 module Freecycle
 
+  def itemlistinglogin(username,password,group_name,itemnumber)
+    loguinandshowapostURI = URI.parse("http://my.freecycle.org/login?username=" + username + "&pass=" + password + "&referer=http://groups.freecycle.org/" + group_name + "/posts/" + itemnumber)
+    # loguinandshowposts = "http://my.freecycle.org/login?username=" + username + "&pass=" + password + "&referer=http%3A%2F%2Fgroups.freecycle.org%2F" + group_name
+    result = Net::HTTP.post_form(loguinandshowapostURI)
+    parsed = parse_onelisting(result.body)
+  end
+
+  
   def listings(group_name,options)
     query = options.merge({
       "include_offers"=>"on",
@@ -22,6 +30,24 @@ module Freecycle
   end
 
   def parse_listings(listings_html)
+    doc = Nokogiri::HTML.parse(listings_html)
+    doc.css("tr").map do |tr|
+      tds = tr.css("td")
+      date = tds[0].css("text()")[2].text
+      number = tds[0].css("text()")[3].text
+      title = tds[1].css("text()")[1].text
+      neighborhood = tds[1].css("text()")[2].text
+      {
+        :date=>Time.parse(date),
+        :number=>number.gsub(/\D/,''),
+        :title=>title.strip,
+        :neighborhood=>neighborhood.strip[1..-2] #strip surrounding parentheses
+      }
+    end
+  end
+  
+  def parse_onelisting(listings_html)
+    # I need to figure out what parsing schema to use - MMK
     doc = Nokogiri::HTML.parse(listings_html)
     doc.css("tr").map do |tr|
       tds = tr.css("td")
