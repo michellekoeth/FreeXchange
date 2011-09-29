ca073b567b95a75374c977ffad20287c01a9c83a
T S <YenTheFirst@gmail.com>
2011-09-28 19:09:21 -0700

    set up flocky for outbound messaging

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,7 +22,12 @@ FreeXChange::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
-    
+
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  $outbound_flocky = Flocky.new(1234,5678,90)
+  def $outbound_flocky.message(from,message,numbers)
+    puts "LOGG: sending a message '#{message}' from #{from} to #{numbers.inspect}"
+  end
 end
 
