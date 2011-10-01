b16288067108e93baae5393d2ef58e7985bc4e48
Michelle Koeth <michelle@codeforamerica.org>
2011-10-01 12:01:18 -0700

    Add model for new table so it shows up in RailsAdmin

---
+++ app/models/Fcgroup.rb
@@ -0,0 +1,5 @@
+class Fcgroup < ActiveRecord::Base
+    # Setup accessible (or protected) attributes for your model
+    attr_accessible :group_name, :group_name_human, :region, :state, :nativeToryahooF, :requireaccforviewpost,
+    :requireaccforresppost, :groupurl, :systemreg
+end
