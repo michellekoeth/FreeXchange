58bc881e9d138c99af6764387ecbcc653bbfb4fc
Michelle Koeth <michelle@codeforamerica.org>
2011-10-01 02:00:34 -0700

    Get rid of requiring users to confirm email w devise. Not every user will have an email adddress

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :lockable, :timeoutable and :activatable
   has_many :searches
   has_many :listings
-  devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :group_name, :phonenumber
--- config/environment.rb
+++ config/environment.rb
@@ -4,13 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 FreeXChange::Application.initialize!
 
-config.action_mailer.deconfig.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => 'gmail.com'
-  :user_name            => 'freexchange1@gmail.com',
-  :password             => ENV['googpass'],
-  :authentication       => 'plain',
-  :enable_starttls_auto => true  }
-
