b9040f34a751b81e69445a8e6761851604f1311a
Michelle Koeth <michelle@codeforamerica.org>
2011-10-02 11:56:09 -0700

    Now detailed listings use safer Mechanize to login before getting listing

--- lib/freecycle.rb
+++ lib/freecycle.rb
@@ -6,20 +6,26 @@ module Freecycle
   def itemlistinglogin(group_name,number)
     password = ENV['FEX']
     fcuser = ENV['FCUSER']
-    link = "http://my.freecycle.org/login?username=" + fcuser + "&pass=" + password +"&referer=http://groups.freecycle.org/" + group_name + "/posts/" + number
-    uri = URI.parse(link)
-    response = Net::HTTP.get_response(uri)
-    # Freecycle is doing a redirect with my above login URL, so we must follow the redirect
-    case response
-      when Net::HTTPSuccess     then response
-      when Net::HTTPRedirection then response = Net::HTTP.get_response(URI.parse(response['location']))
-      else
-        response.error!
-    end
-    puts "uri is : " + link            # => 301
-    parsed = parse_onelisting(response.body)
+    # User Mechanize to login. Some freecycle groups require that you are logged in to view the details of a listing
+    agent = Mechanize.new
+    agent.get("http://my.freecycle.org/login")
+    # The first form is the login form
+    form = agent.page.forms.first
+    form.username = ENV['FCUSER']
+    form.pass = ENV['FEX']
+    form.submit
+    #form.submit # may need to do twice to work
+    form2 = agent.get("http://groups.freecycle.org/" + group_name + "/posts/" + number)
+    trs = agent.page.search("tr") 
+    ret = {"number"=>trs[1].css("td")[0].css("text()")[0].text, 
+            # trs[2] and 3 just lists that it is an offer and other meta data- we can skip
+            "subject"=>trs[4].css("td")[0].css("text()")[0].text,
+            "neighborhood"=>trs[5].css("td")[0].css("text()")[0].text,
+            "description"=>trs[6].css("td")[0].css("text()")[0].text,
+            "date"=>trs[7].css("td")[0].css("text()")[0].text
+          }
   end
-
+  
   def respondtoofferFC(username,password,group_name,itemnumber)
     #MMK to put mechanize code here for responding to an offer which is on the freecycle site itself
   end
@@ -67,19 +73,4 @@ module Freecycle
       }
     end
   end
-  
-  def parse_onelisting(listing_html)
-    # This function will parse out the detailed data for one listing
-    doc = Nokogiri::HTML.parse(listing_html)
-    trs = doc.css("tr") #gives you an array of all the trs
-    # skip the first one - so use the index 1 (index 0 is the first one)
-    ret = {"number"=>trs[1].css("td")[0].css("text()")[0].text, 
-            # tr[2] just lists that it is an offer - we can skip
-            "subject"=>trs[3].css("td")[0].css("text()")[0].text,
-            "neighborhood"=>trs[4].css("td")[0].css("text()")[0].text,
-            "description"=>trs[5].css("td")[0].css("text()")[0].text,
-            "date"=>trs[6].css("td")[0].css("text()")[0].text
-          }
-  end
-
 end
